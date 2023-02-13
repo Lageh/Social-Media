96f76f44b84d635bb5e6e12f28e31df14159febd
Lageh <guilherme0895@gmail.com>
2023-02-13 14:41:19 -0300

    First Commit

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("#login-form");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  username.classList.add("shake");
+  password.classList.add("shake");
+
+  setTimeout(() => {
+    username.classList.remove("shake");
+    password.classList.remove("shake");
+  }, 1000);
+});
+
+function acao() {
+  alert("Login efetuado com sucesso!");
+}
